152dc411a8e51fb83d1af2770ee604114e745824
Waleed Qamar <149614260+waleed719@users.noreply.github.com>
2025-03-11 14:30:10 +0500

    Add files via upload

---
+++ AI_lab_1 BCS23023.py
@@ -0,0 +1,97 @@
+# %%
+print("Hello Wolrd")
+
+
+# %%
+x = 1
+#the initial value of x is 1
+if x > 0:
+    print("There are two comments")     #print a statement
+
+# %%
+txt = input("Type something to test out:")
+print(txt)
+
+# %%
+print("Statement1")
+print("Statement2")
+#You can write two statements on single line
+print("Statement1"); print("Statement2")
+
+# %%
+x = 1
+if x > 0:
+print("There is no indentation")     #print a statement
+
+# %%
+x = 1
+if x > 0:
+ print("There is a single space indentation")
+
+# %%
+x = 1
+if x > 0:
+    print("There is a single tab indentation")
+
+# %%
+x = 1
+if x > 0:
+    print("There is a single space + tab indentation")
+
+# %%
+x = 12
+y = 3.14
+z = 3 + 9j
+has_eaten = False
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(has_eaten))
+
+
+# %%
+str1 = "String1"
+print(str1)
+print(type(str1))
+
+# %%
+#SPECIAL CHARACTERS
+print("This is a backlash (\\) sign")
+print("This is a tab \t sign")
+print("This is a single quotes\' sign")
+print("This is a double quote \" sign")
+print("This is a new line \nNew Line")
+
+
+# %%
+string1 = "PYTHON TUTORIAL"
+print(string1[0])
+print(string1[1])
+print(string1[2])
+print(string1[-5])
+print(string1[12])
+
+# %%
+list1 = [1,2,3,4,5]
+print(list1)
+print(type(list1))
+
+list2 = [1.1,2.2,3.3,4.4,5.5]
+print(list2)
+print(type(list2))
+
+list3 = [100, 'color', True]
+print(list3)
+print(type(list3))
+
+# %%
+#LIST INDICES
+print(list3[2])
+
+# %%
+#LIST SLICES
+print(list1[: 3])
+print(list2[-3: -1])
+
+
